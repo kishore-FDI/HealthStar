27ca1366afd54010f1886c47bd5449c3b9893c7f
Kishore <kishore22705@gmail.com>
2024-07-11 07:25:17 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { PatientForm } from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <div className="flex flex-row justify-between gap-2">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={100}
+            alt="patient"
+            className="mb-8 h-10 w-fit"
+          />
+          <p className="mt-1 text-xl">Health Star</p>
+          </div>
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 Health-Star
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/Screenshot 2024-07-11 055158-fotor-ai-art-effects-20240711055620.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
+  );
+};
+
+export default Home;
